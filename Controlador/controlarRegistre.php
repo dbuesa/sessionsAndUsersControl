fb89d1310ed788d8d3d91d00955bbd8cdff6fb14
David Buesa <d.buesa@sapalomera.cat>
2023-10-18 16:44:58 +0200

    implementació d'errors

--- Controlador/controlarRegistre.php
+++ Controlador/controlarRegistre.php
@@ -1,8 +1,6 @@
 <?php 
 //David Buesa
 
-include "../Vista/register.vista.php";
-
 $errors = array();
 
 if(!empty($_POST["username"]) && !empty($_POST["password"]) && !empty($_POST["password2"])){
@@ -16,14 +14,22 @@ function comprovarContrassenya($contr1, $contr2){
     if($contr1 == $contr2){
         return true;
     }else{
-        $errors[] = "Les contrassenyes no coincideixen";
+        
         return false;
+    }
+}
+function validarContrassenya($contr1){
+    if (strlen($contr1) < 6) {
+        
+        return false;
+    }else{
+        return true;
     }
 }
 
 function comprovarUsuari($user){
-    if (strlen($user) < 4) {
-        $errors[] = "L'usuari ha de tenir més de 4 caràcters";
+    if (strlen($user) < 5) {
+        
         return false;
     }else{
         return true;
@@ -37,18 +43,33 @@ function existeixUsuari($user){
     $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if($result){
-        $errors[] = "L'usuari ja existeix";
         return false;
     }else{
         return true;
     }
 }
 
+if (isset($_POST['signup_submit'])) {
+    if (!comprovarContrassenya($contr1, $contr2)) {
+        $errors[] = "Les contrassenyes no coincideixen";
+    }
+    if (!validarContrassenya($contr1)) {
+        $errors[] = "La contrassenya ha de tenir més de 6 caràcters";
+    }
+    if (!comprovarUsuari($user)) {
+        $errors[] = "L'usuari ha de tenir més de 5 caràcters";
+    }
+    if (!existeixUsuari($user)) {
+        $errors[] = "L'usuari ja existeix";
+    }
 
-if (isset($_POST['signup_submit']) && existeixUsuari($user) && comprovarUsuari($user) && comprovarContrassenya($contr1, $contr2)) {
-    require_once '../Model/register.php';
-    $contrassenya = hash('sha512', $contr1);
-    afegirUsuari($user, $contrassenya);    
+    if (empty($errors)) {
+        require_once '../Model/register.php';
+        $contassenya = password_hash($contr1, PASSWORD_DEFAULT);
+        afegirUsuari($user, $contassenya);
+        header("Location: ../index.php");
+    }
 }
 
+include "../Vista/register.vista.php";
 ?>
--- Vista/register.vista.php
+++ Vista/register.vista.php
@@ -11,33 +11,38 @@
         <div id="login-box">
         <div class="left">
             <h1>Registre</h1>
-            <br><br><br>
+            <br>
             <input type="text" name="username" placeholder="Usuari"  required/>
             <input type="password" name="password" placeholder="Contrassenya" required/>
             <input type="password" name="password2" placeholder="Repeteix la contrassenya" required />
             
+    
             <input type="submit" name="signup_submit" value="Registrar-se" />   
+            <?php
+                if (!empty($errors)) {
+                    echo '<br>';
+                    echo '<ul>';
+                    foreach ($errors as $error) {
+                        echo '<li>' .  $error . "</li>";
+                    }
+                    echo '</ul>';
+                }
+            ?>
         </div>
+        <div>
+    
+    </div>
     </form> 
     <div class="right">
         <a href="../index.php">Tornar</a>
 
-        <span class="loginwith">Sign in with<br />Xarxa social</span>
-        
+        <span class="loginwith"><br /></span>
         <button class="social-signin facebook">Registrar-se via facebook</button>
         <button class="social-signin twitter">Registrar-se via twitter</button>
         <button class="social-signin google">Registrar-se via Google+</button>
     </div>
     <div class="or">O</div>
     </div>
-    <?php
-        if (!empty($errors)) {
-            echo '<ul>';
-            foreach ($errors as $error) {
-                echo '<li>' . $error . '</li>';
-            }
-            echo '</ul>';
-        }
-    ?>
+
 </body>
 </html>
